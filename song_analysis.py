96c9d2b07805304148f6ec8ac0e48a9e0ca8b563
Max Kanter <kmax12@gmail.com>
2014-12-23 16:54:27 -0600

    refactor

--- song_analysis.py
+++ song_analysis.py
@@ -4,8 +4,6 @@ from collections import defaultdict
 import numpy as np 
 import gensim
 
-
-
 def group_by(songs,key,weighted=False):
 	dist = defaultdict(list)
 
@@ -26,53 +24,62 @@ def group_by(songs,key,weighted=False):
 
 	return dist
 
-USE_MODEL_CACHE = True
-MODEL_CACHE_NAME = 'topic_model_cache'
+def process_songs(songs):
+	#process album metadata to get year data
+	albums = json.load(open('album_meta.json', 'r'))['results']['metadata']
+	album_years = {}
+	for a in albums:
+		year = a['year'][1:-1]
+		if year != "":
+			album_years[a['url']] = int(year)
 
-#load in songs
+	#process songs
+	for i,s in enumerate(songs):
+		#add year data to songs
+		if 'album_url' in songs[i]:
+			album_url = songs[i].get('album_url', None)
+			songs[i]['year'] = album_years.get(album_url, None)
+
+		if 'views' in songs[i]:
+			val = songs[i]['views'][:-6].replace(',','')
+			songs[i]['views'] = int(val)
+
+	return songs
+
+
+def make_topic_model(songs_texr, USE_MODEL_CACHE=False, MODEL_CACHE_NAME = 'topic_model_cache'):
+	
+	if not USE_MODEL_CACHE:
+		model = TopicModel()
+		model.build(songs_text)
+		model.save(MODEL_CACHE_NAME)
+	else:
+		print 'Loading topic model from cache'
+		model = TopicModel.load(MODEL_CACHE_NAME)
+
+
+	return model
+
+
+
+
+
+
+#load in and process songs
 songs = json.load(open('songs.json', 'r'))['results']['details']
-
-#process album metadata to get year data
-albums = json.load(open('album_meta.json', 'r'))['results']['metadata']
-album_years = {}
-for a in albums:
-	year = a['year'][1:-1]
-	if year != "":
-		album_years[a['url']] = int(year)
-
-#process songs
-for i,s in enumerate(songs):
-	#add year data to songs
-	if 'album_url' in songs[i]:
-		album_url = songs[i].get('album_url', None)
-		songs[i]['year'] = album_years.get(album_url, None)
-
-	if 'views' in songs[i]:
-		val = songs[i]['views'][:-6].replace(',','')
-		songs[i]['views'] = int(val)
+songs = process_songs(songs)
 
 
-
-#grab just lyric text from songs
-songs_text = [s['lyrics'] for s in songs]
-
-
-if not USE_MODEL_CACHE:
-	model = TopicModel()
-	model.build(songs_text)
-	model.save(MODEL_CACHE_NAME)
-else:
-	print 'Loading topic model from cache'
-	model = TopicModel.load(MODEL_CACHE_NAME)
-
-
+#find topics
+songs_text = songs_text = [s['lyrics'] for s in songs] #grab just lyric text from songs
+model = make_topic_model(songs_text, True)
 topics = model.model_docs(songs_text)
 
 
 #add topic distrubtions back to songs list
 for i,t_dist in enumerate(topics):
 	songs[i]['topic_dist'] = gensim.matutils.sparse2full(t_dist, model.num_topics)
-	songs[i]['weighted_topic_dist'] = gensim.matutils.sparse2full(t_dist, model.num_topics)*songs[i]['views']
+	songs[i]['weighted_topic_dist'] = gensim.matutils.sparse2full(t_dist, model.num_topics)*songs[i]['views'] #unnormailed distribution weighted by song popularity
 
 
 print group_by(songs, 'year')
